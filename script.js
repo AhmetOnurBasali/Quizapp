00801535c602ecf981c93a2695e67f5e0a17e962
ao.basali@gmail.com <you@example.com>
2022-11-20 17:30:57 +0100

    forth

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+
 let questions = [
 
     {
@@ -41,3 +44,45 @@ let questions = [
         "right_answer": 3
     }
 ];
+
+
+let currentQuestion = 0;
+
+
+
+function init() {
+    document.getElementById('all-question').innerHTML = questions.length;
+
+    showQuestion();
+
+}
+
+
+function showQuestion() {
+    let question = questions[currentQuestion];
+
+    document.getElementById('questiontext').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+
+}
+
+
+function answer(a) {
+    let question = questions[currentQuestion];
+
+    let rightAnswer = a.slice(-1);
+
+    if (rightAnswer == question['right_answer']) {
+        console.log('Richtige Antwort!!!')
+    }
+
+    else (
+        console.log('Falsche Antwort!!!')
+
+    )
+
+
+}
