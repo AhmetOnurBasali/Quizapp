14e9215b306cea3a8a0078532eaf83fa289ea6e6
ao.basali@gmail.com <you@example.com>
2022-11-24 18:23:24 +0100

    fifth

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
-
-
 let questions = [
 
     {
@@ -46,55 +44,3 @@ let questions = [
 ];
 
 
-
-
-let currentQuestion = 0;
-
-
-
-function init() {
-    document.getElementById('all-question').innerHTML = questions.length;
-
-    showQuestion();
-
-}
-
-
-function showQuestion() {
-    let question = questions[currentQuestion];
-
-    document.getElementById('questiontext').innerHTML = question['question'];
-    document.getElementById('answer_1').innerHTML = question['answer_1'];
-    document.getElementById('answer_2').innerHTML = question['answer_2'];
-    document.getElementById('answer_3').innerHTML = question['answer_3'];
-    document.getElementById('answer_4').innerHTML = question['answer_4'];
-
-}
-
-
-function answer(a) {
-    let question = questions[currentQuestion];
-
-    let rightAnswer = a.slice(-1);
-
-    let idOfRightAnswer = `answer_${question['right_answer']}`;
-
-
-    
-
-    if (rightAnswer == question['right_answer']) {
-        document.getElementById(a).parentNode.classList.add('bg-success');
-        document.getElementById(a).parentNode.classList.remove('quiz-answer-card');
-    }
-
-    else {
-        document.getElementById(a).parentNode.classList.add('bg-danger');
-        document.getElementById(a).parentNode.classList.remove('quiz-answer-card');
-        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
-
-    }
-
-    document.getElementById('next-button').disabled = false;
-
-
-}
